a8987e66c03d4e6e6d72567766a0feb448aafc33
David Prause <praused@hotmail.com>
2017-09-20 10:44:16 -0400

    Example complete

--- Ch14Ex05_ExtensionMethods/Program.cs
+++ Ch14Ex05_ExtensionMethods/Program.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExtensionLib;//import namespace of the extension method class.
+
+/*
+ * To create and use an extension methods you must do the following:
+ * 1. Create a nongeneric static class.
+ * 2. Add extension methods as static methods to the static class, using extension method syntax:
+ *      a. The method must be static.
+ *      b. The method must include the instance parameter, a parameter representing the instance of the type being extended.
+ *      c. The instance parameter must be the first parameter defined for the method.
+ *      d. The instance parameter must have no modifiers other than the required this keyword.
+ *      template: public static <<ReturnType>> <<ExtensionMethodName>>(this <<TypeToExtend>> <<Instance>>) { ... }
+ * 3. Import the namespace containing the extension method class into the code where you want to use it (with a using statement.)
+ * 4. Call your extension methods through instances of the extended type, just like any other method. 
+ */
 
 namespace Ch14Ex05_ExtensionMethods
 {
@@ -10,6 +24,18 @@ namespace Ch14Ex05_ExtensionMethods
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a string to convert:");
+            string sourceString = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("String with title casing:");
+            Console.WriteLine($"{sourceString.GetWords(capitalizeWords: true).AsSentence()}");
+            Console.WriteLine();
+            Console.WriteLine("String backwards:");
+            Console.WriteLine($"{sourceString.GetWords(reverseOrder: true, reverseWords: true).AsSentence()}");
+            Console.WriteLine();
+            Console.WriteLine("String length backwards:");
+            Console.WriteLine($"{sourceString.Length.ToStringReversed()}");
+            Console.ReadKey();
         }
     }
 }
---
+++ ExtensionLib/WordProcessor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionLib
+{
+    public static class WordProcessor //the extension method class, must be a nongeneric static class.
+    {
+        public static List<string> GetWords(this string sentence, bool capitalizeWords = false, bool reverseOrder = false, bool reverseWords = false)
+        //an extension method, extending string
+        {
+            List<string> words = new List<string>(sentence.Split(' '));
+            if (capitalizeWords) words = CapitalizeWords(words);
+            if (reverseOrder) words = ReverseOrder(words);
+            if (reverseWords) words = ReverseWords(words);
+            return words;
+        }
+
+        public static string ToStringReversed(this object inputObject)
+        //an extension method, extending object
+        {
+            return ReverseWord(inputObject.ToString());
+        }
+
+        public static string AsSentence(this List<string> words)
+        //an extension method, extending List<string>
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int wordIndex = 0; wordIndex < words.Count; wordIndex++)
+            {
+                sb.Append(words[wordIndex]);
+                if (wordIndex != words.Count - 1) sb.Append(' ');
+            }
+            return sb.ToString();
+        }
+
+        private static List<string> ReverseWords(List<string> words)
+        {
+            List<string> reversedWords = new List<string>();
+            foreach (string word in words)
+            {
+                reversedWords.Add(ReverseWord(word));
+            }
+            return reversedWords;
+        }
+
+        private static string ReverseWord(string word)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int characterIndex = word.Length - 1; characterIndex >= 0; characterIndex--)
+            {
+                sb.Append(word[characterIndex]);
+            }
+            return sb.ToString();
+        }
+
+        private static List<string> ReverseOrder(List<string> words)
+        {
+            List<string> reversedOrder = new List<string>();
+            for (int wordIndex = words.Count - 1; wordIndex >= 0; wordIndex--)
+            {
+                reversedOrder.Add(words[wordIndex]);
+            }
+            return reversedOrder;
+        }
+
+        private static List<string> CapitalizeWords(List<string> words)
+        {
+            List<string> capitalizedWords = new List<string>();
+            foreach (string word in words)
+            {
+                if (word.Length == 0) continue;
+                if (word.Length == 1) capitalizedWords.Add(word[0].ToString().ToUpper());
+                else capitalizedWords.Add(word[0].ToString().ToUpper() + word.Substring(1));
+            }
+            return capitalizedWords;
+        }
+    }
+}
